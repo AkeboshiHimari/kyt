4f3aa3279b694094bd40e072c2db732cb4c50432
L2 <hello@akeboshi.cc>
2025-09-08 03:27:47 +0900

    fix: auth redirection error

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,26 +3,15 @@ import { cookies } from 'next/headers'
 import { createClient } from '@/utils/supabase/server'
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
-  const code = searchParams.get('code')
-  // A redirect destination can be included as a ?next= parameter
-  // If next is not present, default to /menu
-  const next = searchParams.get('next') ?? '/menu'
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
 
   if (code) {
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
-    
-    cookieStore.getAll()
-    
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
-
-    if (!error) {
-      // URL to redirect to after sign in process completes
-      return NextResponse.redirect(`${origin}${next}`)
-    }
+    await supabase.auth.exchangeCodeForSession(code)
   }
 
-  // URL to redirect to if the sign in process fails
-  return NextResponse.redirect('/auth/auth-code-error')
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(requestUrl.origin)
 }
